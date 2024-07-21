dbfb9651754b889c4de6a7902b6ca3bd3e78568c
sajjad-FL <sajjad.alam@fissionlabs.com>
2024-07-21 07:31:59 +0530

    'back-camera'

--- src/components/UI/WebcamCapture.js
+++ src/components/UI/WebcamCapture.js
@@ -1,17 +1,45 @@
 // WebcamModal.js
-import React, { useCallback, useRef } from 'react';
+import { useWindowSize } from "@uidotdev/usehooks";
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 import './style.css';
-
 const WebcamModal = ({ isOpen, onClose, onCapture }) => {
-    console.log({isOpen, onclose, onCapture})
+    console.log({ isOpen, onclose, onCapture })
     const webcamRef = useRef(null);
+    const size = useWindowSize();
 
+    const isLandscape = size.height <= size.width;
+    const ratio = isLandscape ? size.width / size.height : size.height / size.width;
     const capture = useCallback(() => {
         const imageSrc = webcamRef.current.getScreenshot();
         onCapture(imageSrc);
         onClose();
     }, [webcamRef, onCapture, onClose]);
+
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+
+    useEffect(() => {
+        // Handler to call on window resize
+        function handleResize() {
+            // Set window width/height to state
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+
+        // Add event listener
+        window.addEventListener("resize", handleResize);
+
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+
+        // Remove event listener on cleanup
+        return () => window.removeEventListener("resize", handleResize);
+    }, [])
 
     if (!isOpen) return null;
 
@@ -23,7 +51,7 @@ const WebcamModal = ({ isOpen, onClose, onCapture }) => {
                     audio={false}
                     ref={webcamRef}
                     screenshotFormat="image/jpeg"
-                    videoConstraints={{ facingMode: { exact: "environment" } }}
+                    videoConstraints={{ facingMode: { exact: "environment" }, aspectRatio: ratio }}
                     style={{ width: '100%', height: '100%' }}
                 />
                 <button onClick={capture} className="webcamp-capture-button">Capture</button>
